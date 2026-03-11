26bb47c416eb6b14cda6a89a2823f3727dbc0d36
vvddalfmr <vvddalfmr@gmail.com>
2026-03-11 17:52:46 +0900

    [Gold IV] Title: 미로만들기, Time: 48 ms, Memory: 35508 KB -BaekjoonHub

---
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -0,0 +1,34 @@
+import heapq
+
+def hq(val, xy):
+
+    pq = []
+    heapq.heappush(pq, [val, xy])
+    while pq:
+        tmp = heapq.heappop(pq)
+        cost = tmp[0]
+        if tmp[1][0] == n-1 and tmp[1][1] == n-1:
+            return tmp[0]
+
+        for dx, dy in [[1, 0], [0, 1], [-1, 0], [0, -1]]:
+            nx = tmp[1][0] + dx
+            ny = tmp[1][1] + dy
+            if 0 <= nx < n and 0 <= ny < n:
+                if arr[nx][ny] == 0:
+                    new_cost = cost + 1
+                else:
+                    new_cost = cost
+                if dist[nx][ny] == -1 or dist[nx][ny] > tmp[0]:
+                    dist[nx][ny] = tmp[0]
+                    heapq.heappush(pq, [new_cost, [nx, ny]])
+                else:
+                    if dist[nx][ny] == -1 or dist[nx][ny] > tmp[0]:
+                        dist[nx][ny] = tmp[0]
+                        heapq.heappush(pq, [new_cost, [nx, ny]])
+
+
+n = int(input())
+arr = [list(map(int, input())) for _ in range(n)]
+dist = [[-1] * n for _ in range(n)]
+
+print(hq(0, [0, 0]))
